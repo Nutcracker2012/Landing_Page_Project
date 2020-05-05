b4318695135a7da8f6d6a30700696cc88bf91e7e
Qian Pan <panqian.la@gmail.com>
2020-05-04 23:04:34 -0400

    Add Dyn Nav

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # Landing Page Project
-
+This project is the second project for udacity front-end nano degree course
 ## Table of Contents
 
 * [Instructions](#instructions)
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+The dynamic navigation
+the dynamic active class
+the scroll events
--- js/app.js
+++ js/app.js
@@ -16,15 +16,20 @@
 /**
  * Define Global Variables
  * 
+ * 
  */
+// store the variable for access later
 
+//nagivation global var
+const navigation = document.querySelector("#navbar__list");
+//section global var
+const sections = document.querySelectorAll('section');
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
  */
-
 
 
 /**
@@ -35,8 +40,49 @@
 
 // build the nav
 
-// Add class 'active' to section when near top of viewport
+const navBuilder = () => {
 
+    let navUI = '';
+    // looping over all sections
+    sections.forEach(section => {
+
+        let sectionID = section.getAttribute("id");
+
+        let sectionDataNav = section.getAttribute("data-nav");
+
+        navUI += `<li><a class="menu__link" href="#${sectionID}">${sectionDataNav}</a></li>`;
+
+    });
+    // append all elements to the naviation
+    navigation.innerHTML = navUI;
+}
+
+navBuilder();
+
+
+
+
+
+
+
+
+
+
+// Add class 'active' to section when near top of viewport
+// Add Active Class to current element
+
+// Get the container element
+// const navContainer = document.getElementById("navbar__list");
+// // Get all buttons with class="btn" inside the container
+// const sections = btnContainer.getElementsByClassName("section");
+// // Loop through the buttons and add the active class to the current/clicked button
+// for (const i = 0; i < sections.length; i++) {
+//     sections[i].addEventListener("click", function() {
+//         const current = document.getElementsByClassName("active");
+//         current[0].className = current[0].className.replace(" active", "");
+//         this.className += " active";
+//     });
+// }
 
 // Scroll to anchor ID using scrollTO event
 
